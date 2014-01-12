c9f43819034c949928afe0d7b2aeda5467458a1f
Reto Zingg <g.d0b3rm4n@gmail.com>
2014-01-12 22:24:23 +0200

    add quazip lib
    source: http://svn.code.sf.net/p/quazip/code/trunk
    last commit: r181 | 2013-12-18 17:55:41 +0200

---
+++ quazip/qztest/testquazip.h
@@ -0,0 +1,17 @@
+#ifndef QUAZIP_TEST_QUAZIP_H
+#define QUAZIP_TEST_QUAZIP_H
+
+#include <QObject>
+
+class TestQuaZip: public QObject {
+    Q_OBJECT
+private slots:
+    void getFileList_data();
+    void getFileList();
+    void add_data();
+    void add();
+    void setFileNameCodec_data();
+    void setFileNameCodec();
+};
+
+#endif // QUAZIP_TEST_QUAZIP_H
